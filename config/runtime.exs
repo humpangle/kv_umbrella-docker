a4f295d3c2064ab2cd10748ad7aa25b4850adf49
Kanmii <maneptha@gmail.com>
2022-07-30 07:49:59 +0200

    Set RELEASE_NAME host to docker service `prod` and use this to set ROUTING_TABLE env variable
    For single host docker-compose cluster, the `prod` service from
    docker-compose.yaml is used as host name. And since all part of
    RELEASE_NODE can be derived from this host name and environment
    variable, we can then set ROUTING_TABLE environment variable.

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,10 +2,10 @@ import Config
 
 routing_table =
   if config_env() == :prod do
-    [
-      {?a..?m, :"server_storage@ex-p"},
-      {?n..?z, :"storage@ex-p"},
-    ]
+    "ROUTING_TABLE"
+    |> System.fetch_env!()
+    |> Base.decode64!()
+    |> :erlang.binary_to_term()
   else
     [{?a..?z, node()}]
   end
